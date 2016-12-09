ec112d7ae46da97190395f414164390603e3981f
Zachary Robbins <xaqiri@outlook.com>
2016-12-09 13:04:06 -0600

    Displays search results as links

--- js/index.js
+++ js/index.js
@@ -1,15 +1,35 @@
 'use strict'
 
-const popResults = () => {
-  for (let i = 0; i < 5; i++) {
-    $('#results').append('<h1>hello</h1')
+const popResults = (results, pages, baseURL) => {
+  $('#results').empty()
+  for (let i = 0; i < 15; i++) {
+    let title = pages[results[i]].title
+    let r = $('<h1>' + title + '</h1>')
+    $('#results').append(r)
+    $(r).wrap(() => {
+      let link = $('<a/>')
+      link.attr('href', baseURL + title)
+      return link
+    })
   }
 }
 
 $(document).ready(() => {
-  popResults()
-  let random = 'https://en.wikipedia.org/wiki/Special:Random'
-  $('#title').on('click', () => {
-    $('#title').html(random)
+  const apiURL = 'https://en.wikipedia.org/w/api.php'
+  const baseURL = 'https://en.wikipedia.org/wiki/'
+  const random = baseURL + 'Special:Random'
+  let searchTerm = ''
+  let baseQuery = apiURL + '?action=query&format=json&gsrlimit=15&generator=search&origin=*&gsrsearch='
+  let results = []
+  //popResults()
+  $('#searchButton').on('click', () => {
+    searchTerm = $('#searchBar').val()
+    $.getJSON(baseQuery + searchTerm, (data) => {
+      results = Object.keys(data.query.pages)
+      popResults(results, data.query.pages, baseURL)
+    })
+  })
+  $('#randomButton').on('click', () => {
+    window.open(random)
   })
 })
