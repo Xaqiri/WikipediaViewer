17da954000bb06f813549e4ece869a02e24922cc
Zachary Robbins <xaqiri@outlook.com>
2016-12-09 17:32:45 -0600

    Cards now show titles and excerpts

--- js/index.js
+++ js/index.js
@@ -1,33 +1,42 @@
 'use strict'
 
 const popResults = (results, pages, baseURL) => {
-  $('#results').empty()
   for (let i = 0; i < 15; i++) {
-    let title = pages[results[i]].title
-    let r = $('<h1>' + title + '</h1>')
+    let page = pages[results[i]]
+    let title = page.title
+    let extract = page.extract
+    let r = $('<div id=resultCard></div>')
+    let rt = $('<h2 id=resultTitle>' + title + '</h2>')
+    let rb = $('<h4 id=resultBody>' + extract + '</h4>')
     $('#results').append(r)
+    $(r).append(rt)
+    $(r).append(rb)
     $(r).wrap(() => {
       let link = $('<a/>')
       link.attr('href', baseURL + title)
+      link.attr('target', '_blank')
       return link
     })
   }
 }
 
+$('#searchBar').val('')
 $(document).ready(() => {
   const apiURL = 'https://en.wikipedia.org/w/api.php'
   const baseURL = 'https://en.wikipedia.org/wiki/'
   const random = baseURL + 'Special:Random'
   let searchTerm = ''
-  let baseQuery = apiURL + '?action=query&format=json&gsrlimit=15&generator=search&origin=*&gsrsearch='
+  let baseQuery = apiURL + '?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=extracts&exintro&explaintext&exsentences=1&exlimit=max&origin=*&gsrsearch='
   let results = []
-  //popResults()
   $('#searchButton').on('click', () => {
+    $('#results').empty()
     searchTerm = $('#searchBar').val()
-    $.getJSON(baseQuery + searchTerm, (data) => {
-      results = Object.keys(data.query.pages)
-      popResults(results, data.query.pages, baseURL)
-    })
+    if (searchTerm) {
+      $.getJSON(baseQuery + searchTerm, (data) => {
+        results = Object.keys(data.query.pages)
+        popResults(results, data.query.pages, baseURL)
+      })
+    }
   })
   $('#randomButton').on('click', () => {
     window.open(random)
